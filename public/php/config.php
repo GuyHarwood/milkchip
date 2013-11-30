c33697ff4420ec104c09bfc2349a1c7ae8e41e7a
MrBaldman <gpharwood@gmail.com>
2013-11-30 10:50:44 +0000

    remove unwanted sections

--- public/php/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/* ---------------------------------------------------------------------- */
-/*	Contact Form
-/* ---------------------------------------------------------------------- */
-
-// The email address where all contact form messages are sent (inbox)
-$your_email_address = 'replaceme@withyourmailaddress.com';
-
